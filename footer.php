ab091f12d9f007cfba66665314231133404691e5
roy-dela-torre <delatorreroy.pdm@gmail.com>
2023-10-06 14:24:20 +0800

    update

--- footer.php
+++ footer.php
@@ -4,7 +4,51 @@
     <div class="container-fluid">
         <div class="wrapper">
             <div class="row">
-
+                <div class="col">
+                    <div class="logo">
+                        <img src="<?php echo get_stylesheet_directory_uri();?>/assets/img/homepage/footer-logo.png" alt="">
+                    </div>
+                </div>
+                <div class="col">
+                    <h5>Quick links</h5>
+                    <div class="row">
+                        <div class="col-6"><a href="http://" target="_blank" rel="noopener noreferrer">home</a></div>
+                        <div class="col-6"><a href="http://" target="_blank" rel="noopener noreferrer">About</a></div>
+                        <div class="col-6"><a href="http://" target="_blank" rel="noopener noreferrer">Articles</a></div>
+                        <div class="col-6"><a href="http://" target="_blank" rel="noopener noreferrer">Contact us</a></div>
+                        <div class="col-6"><a href="http://" target="_blank" rel="noopener noreferrer">faqs</a></div>
+                    </div>
+                </div>
+                <div class="col">
+                    <h5>trading Cards</h5>
+                    <div class="row">
+                        <div class="col-4"><a href="http://" target="_blank" rel="noopener noreferrer">flesh and blood</a></div>
+                        <div class="col-4"><a href="http://" target="_blank" rel="noopener noreferrer">pokémon</a></div>
+                        <div class="col-4"><a href="http://" target="_blank" rel="noopener noreferrer">Yu-Gi-Oh!</a></div>
+                        <div class="col-4"><a href="http://" target="_blank" rel="noopener noreferrer">Magic</a></div>
+                        <div class="col-4"><a href="http://" target="_blank" rel="noopener noreferrer">Digimon</a></div>
+                        <div class="col-4"><a href="http://" target="_blank" rel="noopener noreferrer">One peace</a></div>
+                        <div class="col-4"><a href="http://" target="_blank" rel="noopener noreferrer">Cardfight!! Vanguard</a></div>
+                        <div class="col-4"><a href="http://" target="_blank" rel="noopener noreferrer">more cards</a></div>
+                    </div>
+                </div>
+                <div class="col">
+                    <h5>Policies</h5>
+                    <div class="row">
+                        <div class="col-6"><a href="http://" target="_blank" rel="noopener noreferrer">Terms and Condition</a></div>
+                        <div class="col-6"><a href="http://" target="_blank" rel="noopener noreferrer">Shipping Policy</a></div>
+                        <div class="col-6"><a href="http://" target="_blank" rel="noopener noreferrer">Refund Policy</a></div>
+                        <div class="col-6"><a href="http://" target="_blank" rel="noopener noreferrer">privacy policy</a></div>
+                    </div>
+                </div>
+                <div class="col">
+                    <h4>Interested in selling your cards?</h4>
+                    <a href="http://" target="_blank" rel="noopener noreferrer" class="sale-with-us">sell with us</a>
+                </div>
+                <div class="bottom">
+                    <hr>
+                    <p class="copyr-right text-uppercase"><b>Copyright © 2022 - <?php echo date('Y')?> TCG.ph | <a href="https://seo-hacker.com/seo-philippines/" target="_blank" rel="noopener noreferrer">SEO</a> <a href="https://seo-hacker.com/" target="_blank" rel="noopener noreferrer"> by SEO-Hacker</a>. Optimized and maintained by <a href="https://sean.si/" target="_blank" rel="noopener noreferrer">Sean Si</a></b></p>
+                </div>
             </div>
         </div>
     </div>
