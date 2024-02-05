486ccfd5282333fcfbcdabf773489139283d4101
Sh-VicZapico <vic.zapico@seo-hacker.net>
2024-02-05 17:44:01 +0800

    search page

--- page-my-account.php
+++ page-my-account.php
@@ -1,9 +1,9 @@
 <?php get_header();
 /*Template Name: My Account*/
 $imgPath = get_stylesheet_directory_uri().'/assets/img/homepage/';
-if (!is_user_logged_in()): ?>
-  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/inc/css/my-account.css">
-  <section class="my-account">
+//if (!is_user_logged_in()): ?>
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/inc/css/desktop/my-account.css">
+  <section class="my_account">
     <div class="container-fluid">
       <div class="row">
         <div class="offset-xl-1 col-xl-10">
@@ -15,8 +15,8 @@ if (!is_user_logged_in()): ?>
     </div>
   </section>
 
-<?php else: ?>
-  <section class="login">
+<?php //else: ?>
+  <!-- <section class="login">
     <div class="container-fluid">
         <div class="wrapper">
             <div class="row">
@@ -25,26 +25,26 @@ if (!is_user_logged_in()): ?>
                         <div class="content-container">
                             <h2 class="text-uppercase text-white">Welcome</h2>
                             <p class="text-white">Log in your account</p>
-                            <?php echo do_shortcode('[custom_login_form]'); ?>
+                            <?php //echo do_shortcode('[custom_login_form]'); ?>
                         </div>
                     </div>
                 </div>
                 <div class="col-xl-2 col-lg-12">
                     <div class="contact-details">
                         <h4 class="text-white text-uppercase">contact details:</h4>
-                        <a href="tel:63+ 092123456789"><img loading="lazy" src="<?php echo $imgPath; ?>telephone.png" alt=""> 63+ 092123456789</a>
-                        <a href="mailto:loremipsum@gmail.com"><img loading="lazy" src="<?php echo $imgPath; ?>email.png" alt="">loremipsum@gmail.com</a>
-                        <a target="_blank" rel="noopener noreferrer"><img loading="lazy" src="<?php echo $imgPath; ?>fb.png" alt="">lorem ipsum</a>
-                        <a href="http://" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="<?php echo $imgPath; ?>instagram.png" alt="">lorem ipsum</a>
+                        <a href="tel:63+ 092123456789"><img loading="lazy" src="<?php //echo $imgPath; ?>telephone.png" alt=""> 63+ 092123456789</a>
+                        <a href="mailto:loremipsum@gmail.com"><img loading="lazy" src="<?php //echo $imgPath; ?>email.png" alt="">loremipsum@gmail.com</a>
+                        <a target="_blank" rel="noopener noreferrer"><img loading="lazy" src="<?php //echo $imgPath; ?>fb.png" alt="">lorem ipsum</a>
+                        <a href="http://" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="<?php //echo $imgPath; ?>instagram.png" alt="">lorem ipsum</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</section>
+</section> -->
   <?php
   // header('Location: ' . get_home_url() . '/register');
-endif; ?>
+//endif; ?>
 <?php
 get_footer();
 ?>
--- search.php
+++ search.php
@@ -1,58 +1,77 @@
 <?php
 get_header();
 global $wp_query;
-$s = $_GET['s'];
+$s = sanitize_text_field($_GET['s']); // Sanitize the search term
+
 ?>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/inc/css/desktop/search.css">
+
 <section class="banner">
     <div class="container-fluid">
         <div class="row">
-            <h1 class="textt-center text-white">Search Results for: "<?php echo $s;?>"</h1>
+            <h1 class="text-center text-white">Search Results for: "<?php echo esc_html($s); ?>"</h1>
         </div>
     </div>
 </section>
-<section class="searchResults" id="four-o-four">
-    <div class="container">
-        <hr>
-        <div class="row">
-            <?php
-            $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-            $args = array(
-                's' => $s,
-                'paged' => $paged,
-                'posts_per_page' => 3,
-                'post_type' => 'post', 
-            );
-            $query = new WP_Query($args);
-            if ($query->have_posts()):
-                while ($query->have_posts()):
-                    $query->the_post();
-                    $searchID = get_the_ID();
-                    $image_id = get_post_thumbnail_id($searchID);
-                    ?>
-                    <div class="col-lg-4 col-md-6 col-sm-12">
-                        <a href="<?php echo the_permalink(); ?>" target="_blank" rel="noopener noreferrer">
-                            <div class="content">
-                                <div class="img-container">
-                                    <img src="<?php echo get_the_post_thumbnail_url($searchID); ?>" alt="<?php echo get_the_title(); ?>" class="img-fluid">
+
+<section class="searchResults">
+    <div class="container-fluid">
+        <div class="wrapper">
+            <div class="row">
+                <?php
+                $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+                $args = array(
+                    's' => $s,
+                    'paged' => $paged,
+                    'posts_per_page' => 8
+                    );
+                $query = new WP_Query($args);
+
+                if ($query->have_posts()):
+                    while ($query->have_posts()):
+                        $query->the_post();
+                        $searchID = get_the_ID();
+                        $image_id = get_post_thumbnail_id($searchID);
+
+                        ?>
+                        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+                            <a href="<?php echo esc_url(get_permalink()); ?>" target="_blank" rel="noopener noreferrer">
+                                <div class="content">
+                                    <div class="img-container">
+                                        <?php
+                                        if ($image_id) {
+                                            $image_url = get_the_post_thumbnail_url($searchID);
+                                            ?>
+                                            <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr(the_title_attribute('echo=0')); ?>" class="img-fluid">
+                                            <?php
+                                        } else {
+                                            // Display a placeholder image or default image if no featured image is available
+                                            ?>
+                                            <img src="http://localhost/tcg/wp-content/uploads/woocommerce-placeholder.png" alt="Placeholder Image" class="img-fluid">
+                                            <?php
+                                        }
+                                        ?>
+                                    </div>
+                                    <div class="content-container">
+                                        <p class="post-type"></p>
+                                        <h3><?php echo esc_html(get_the_title()); ?></h3>
+                                        <p><?php echo wp_trim_words(get_the_content(), 10); ?></p>
+                                        <button class="view-product">
+                                            view product
+                                        </button>
+                                    </div>
                                 </div>
-                                <div class="content-container">
-                                    <p class="post-type"></p>
-                                    <h3><?php echo the_title(); ?></h3>
-                                    <?php echo wp_trim_words(get_the_content(), 10); ?>
-                                    <button class="view-product">
-                                        view product
-                                    </button>
-                                </div>
-                            </div>
-                        </a>
+                            </a>
+                        </div>
+                    <?php endwhile; ?>
+                    <div class="pagination-link d-none">
+                        <?php echo paginate_links(array('total' => $query->max_num_pages, 'prev_next' => false)); ?>
                     </div>
-                <?php endwhile; ?>
-                <div class="pagination-link">
-                    <?php echo paginate_links(array('total' => $query->max_num_pages, 'prev_next' => false)); ?>
-                </div>
-                <?php wp_reset_query();
-            endif; ?>
+                    <?php wp_reset_query();
+                endif; ?>
+            </div>
         </div>
     </div>
 </section>
+
 <?php get_footer(); ?>
